eecad478d51f7f4f06e67808a26a4bf89584dc1b
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-23 12:56:24 -0500

    import and implement layout css

--- reactburgerbuilder/src/components/Layout/Layout.js
+++ reactburgerbuilder/src/components/Layout/Layout.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
 import Auxillary from '../../higherOrderComponents/Auxillary';
+import classes from './Layout.css';
 
 const layout = ( props ) => (
     <Auxillary>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <div >Toolbar, SideDrawer, Backdrop</div>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Auxillary>
