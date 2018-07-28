dc140f6ee2e50de7d8f7d39a2dc1a95e08b597ba
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-27 23:22:08 -0500

    changed in to class from function to demonstrate componentWill Update

--- reactburgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
+++ reactburgerbuilder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Auxillary from '../../../higherOrderComponents/Auxillary';
 import Button from '../../UserInterface/Button/Button';
 
-const orderSummary = (props) => {
-    const ingredientSummary = Object.keys(props.ingredients)
+class OrderSummary extends Component {
+
+    componentWillUpdate() {
+        console.log('[Order Summary] Will Update');
+    }
+
+    render(){
+        const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey =>{
             return (
                 <li key={igKey}>
-                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {props.ingredients[igKey]}
+                    <span style={{textTransform: 'capitalize'}}>{igKey}</span>: {this.props.ingredients[igKey]}
                 </li>);
         });
-    return (
-        <Auxillary>
+        return(
+            <Auxillary>
             <h3>Your Order</h3>
             <p>Delicious burger with the following ingredients: </p>
             <ul>
                 {ingredientSummary}
             </ul>
-            <p><strong>Total Price: {props.price}</strong></p>
+            <p><strong>Total Price: {this.props.price}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType='Danger' clicked={props.purchaseCanceled}>CANCEL</Button>
-            <Button btnType='Success' clicked={props.purchaseContinue}>CONTINUE</Button>
+            <Button btnType='Danger' clicked={this.props.purchaseCanceled}>CANCEL</Button>
+            <Button btnType='Success' clicked={this.props.purchaseContinue}>CONTINUE</Button>
         </Auxillary>
-    );
+        );
+    }
+}
 
-};
-
-export default orderSummary;
+export default OrderSummary;
