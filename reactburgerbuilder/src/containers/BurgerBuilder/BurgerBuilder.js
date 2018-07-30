88b64cef8ff2f32dac3fa4be0dc1467ff4451186
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-07-30 10:08:33 -0500

    imported axios and created purchase handler that uses axios instance and firebase

--- reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,7 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UserInterface/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
     salad:  0.5,
@@ -78,7 +79,24 @@ class BurgerBuilder extends Component{
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        // alert('You continue!');
+        const order  = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Jeridd',
+                address: {
+                    street: '123TestStreet',
+                    zipCode: '54321',
+                    contry: 'States'
+                },
+                email: 'test@test.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+        .then(response => console.log(response))
+        .catch(error => console.log(error));
     }
 
     render() {
