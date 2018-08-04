e529c6707fe6ffb3d114d257c25d5d3245477aaf
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-03 20:27:23 -0500

    commented out the old purchase handler to push the new routed one

--- reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -90,28 +90,29 @@ class BurgerBuilder extends Component{
 
     purchaseContinueHandler = () => {
         // alert('You continue!');
-        this.setState( { loading: true } );
-        const order  = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Jeridd',
-                address: {
-                    street: '123TestStreet',
-                    zipCode: '54321',
-                    contry: 'States'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
-        }
-        axios.post('/orders.json', order)
-            .then(response => {
-                this.setState({loading: false, purchasing: false});
-        })
-        .catch(error => {
-            this.setState({loading: false, purchasing: false});
-        });
+        // this.setState( { loading: true } );
+        // const order  = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Jeridd',
+        //         address: {
+        //             street: '123TestStreet',
+        //             zipCode: '54321',
+        //             contry: 'States'
+        //         },
+        //         email: 'test@test.com'
+        //     },
+        //     deliveryMethod: 'fastest'
+        // }
+        // axios.post('/orders.json', order)
+        //     .then(response => {
+        //         this.setState({loading: false, purchasing: false});
+        // })
+        // .catch(error => {
+        //     this.setState({loading: false, purchasing: false});
+        // });
+        this.props.history.push('/checkout');
     }
 
     render() {
