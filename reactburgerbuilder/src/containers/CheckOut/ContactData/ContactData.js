d2a9dc5f7ba2fd55115c976e16deea38fc41ca28
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-06 08:23:55 -0500

    added long form of input orderform with the config and type

--- reactburgerbuilder/src/containers/CheckOut/ContactData/ContactData.js
+++ reactburgerbuilder/src/containers/CheckOut/ContactData/ContactData.js
@@ -8,11 +8,57 @@ import Input from '../../../components/UserInterface/Input/Input';
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street'
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP Code'
+                },
+                value: ''
+            },
+            contry: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Contry'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [
+                        {value: 'fastest', displayValue: 'Fastest'},
+                        {value: 'cheapest', displayValue: 'Cheapest'},
+                    ],
+                },
+                value: ''
+            },
         },
         loading: false
     }
@@ -23,16 +69,6 @@ class ContactData extends Component {
         const order  = {
             ingredients: this.props.ingredients,
             price: this.props.price,
-            customer: {
-                name: 'Jeridd',
-                address: {
-                    street: '123TestStreet',
-                    zipCode: '54321',
-                    contry: 'States'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
         }
         axios.post('/orders.json', order)
             .then(response => {
@@ -47,7 +83,7 @@ class ContactData extends Component {
     render() {
         let form = (
             <form>
-                <Input inputtype="input" type='text' name='name' placeholder='Your Name' />
+                <Input elementType='...' elementConfig='...' value='...' />
                 <Input inputtype="input" type='email' name='email' placeholder='Your email' />
                 <Input inputtype="input" type='text' name='street' placeholder='Street' />
                 <Input inputtype="input" type='text' name='postal' placeholder='Postal Code' />
