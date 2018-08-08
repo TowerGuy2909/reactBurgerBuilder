8cbcbf2da83e6a23e97bf163fd55b3c3109d915e
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-08 15:38:05 -0500

    added action types with switch cases to add or remove 1 ingredient

--- reactburgerbuilder/src/store/reducer.js
+++ reactburgerbuilder/src/store/reducer.js
@@ -1,2 +1,36 @@
-export const ADD_INGREDIENT = 'ADD_INGREDIENT';
-export const REMOVE_INGREDIENT = 'REMOVE_INGREDIENT';
+import * as actionTypes from './actions';
+
+const initialState = {
+    ingredients: {
+        salad: 0,
+        bacon: 0,
+        cheese: 0,
+        meat: 0
+    },
+    totalPrice: 4
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case actionTypes.ADD_INGREDIENT:
+            return{
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] + 1
+                }
+            };
+        case actionTypes.REMOVE_INGREDIENT:
+            return{
+                ...state,
+                ingredients: {
+                    ...state.ingredients,
+                    [action.ingredientName]: state.ingredients[action.ingredientName] - 1
+                }
+            };
+        default:
+            return state;
+    }
+};
+
+export default reducer;
