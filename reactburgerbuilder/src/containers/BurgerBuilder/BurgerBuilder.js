32da40f3742ead362bf5cb2b2ac2098a61497c70
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-08 20:41:18 -0500

    redoing purchasable

--- reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ reactburgerbuilder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -16,7 +16,7 @@ import * as actionTypes from '../../store/actions';
 
 class BurgerBuilder extends Component{
    state = {
-       purchasable: false,
+     
        purchasing: false,
        loading: false,
        error: false
@@ -42,7 +42,7 @@ class BurgerBuilder extends Component{
             .reduce((sum, el) => {
                 return sum + el;
             }, 0);
-        this.setState({purchasable: sum > 0});
+        return sum > 0;
     }
 
     purchaseHandler = () => {
@@ -86,7 +86,7 @@ class BurgerBuilder extends Component{
                         ingredientAdded={this.props.onIngredientAdded} 
                         ingredientRemoved={this.props.onIngredientRemove}
                         disabled = {disabledInfo}
-                        purchasable = {this.state.purchasable}
+                        purchasable = {this.updatePurchaseState(this.props.ings)}
                         ordered = {this.purchaseHandler} 
                         price = {this.props.price}/>
                 </Auxillary> 
