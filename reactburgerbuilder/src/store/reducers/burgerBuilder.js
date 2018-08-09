49ed9b6218cfd985d0ce058aebd7b8e98008bb12
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-09 13:17:36 -0500

    created action types for setting ingredients and a error to fetch them

--- reactburgerbuilder/src/store/reducers/burgerBuilder.js
+++ reactburgerbuilder/src/store/reducers/burgerBuilder.js
@@ -24,6 +24,17 @@ const reducer = (state = initialState, action) => {
                 },
                 totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
             };
+        case actionTypes.SET_INGREDIENTS:
+            return {
+                ...state,
+                ingredients: action.ingredients,
+                error: false
+            }
+        case actionTypes.FETCH_INGREDIENTS_FAILED:
+            return {
+                ...state,
+                error: true
+            }
         case actionTypes.REMOVE_INGREDIENT:
             return{
                 ...state,
