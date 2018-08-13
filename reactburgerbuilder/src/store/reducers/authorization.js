240400cd6267d1a7443d426a18a9c51785f5ed43
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-13 17:56:51 -0500

    action and creator for logout

--- reactburgerbuilder/src/store/reducers/authorization.js
+++ reactburgerbuilder/src/store/reducers/authorization.js
@@ -10,7 +10,7 @@ const initialState = {
 
 const authorizationStart = (state, action) => {
     return updateObject(state, {error: null, loading: true});
-}
+};
 
 const authorizationSuccess = (state, action) => {
     return updateObject(state, {
@@ -25,16 +25,21 @@ const authorizationFail = (state,action) => {
     return updateObject(state, {
         error: action.error,
         loading: false
-    })
-}
+    });
+};
+
+const authorizationLogout = (state, action) => {
+    return updateObject(state, {token: null, userId: null});
+};
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case actionTypes.AUTHORIZATION_START: return authorizationStart(state, action);
         case actionTypes.AUTHORIZATION_SUCCESS: return authorizationSuccess(state, action);
-        case actionTypes.AUTHORIZATION_FAIL: return authorizationFail(state, action);   
+        case actionTypes.AUTHORIZATION_FAIL: return authorizationFail(state, action);
+        case actionTypes.AUTHORIZATION_LOGOUT: return authorizationLogout(state, action);   
         default: return state;
-    }
-}
+    };
+};
 
 export default reducer;
