45cdc46678fb548185b6bd64829e30d6da13497b
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-13 01:29:44 -0500

    seting up signing up on firebase

--- reactburgerbuilder/src/store/actions/authorization.js
+++ reactburgerbuilder/src/store/actions/authorization.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 import * as actionTypes from './actionTypes';
 
 export const authorizationStart = () => {
@@ -23,5 +25,19 @@ export const authorizationFail = (error) => {
 export const authorization = (email, password) => {
     return dispatch => {
         dispatch(authorizationStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        };
+        axios.post('https://www.googleapis.com/identitytoolkit/v3/relyingparty/signupNewUser?key=AIzaSyAejy37NPoM-mlG4JYV63SG38KC_FQT5ZM', authData)
+            .then(response => {
+                console.log(response);
+                dispatch(authorizationSuccess(response.data));
+            })
+            .catch(err => {
+                console.log(err);
+                dispatch(authorizationFail(err));
+            })
     }
 }
