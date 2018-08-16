cd8f16ca7aece9540812d863076117da7fa468b9
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-16 00:00:04 -0500

    exporting creator for seting path while building burger

--- reactburgerbuilder/src/store/actions/authorization.js
+++ reactburgerbuilder/src/store/actions/authorization.js
@@ -60,3 +60,10 @@ export const authorization = (email, password, isSignup) => {
             })
     }
 }
+
+export const setAuthRedirectPath = (path) => {
+    return {
+        type: actionTypes.SET_AUTH_REDIRECT_PATH,
+        path: path
+    }
+}
