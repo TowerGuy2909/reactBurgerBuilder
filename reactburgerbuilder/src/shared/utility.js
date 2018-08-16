17cab5e03c12a62ef7bef03845c1f3dbbc2eedab
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-16 14:11:26 -0500

    added checkValidity to outsource it to multiple places

--- reactburgerbuilder/src/shared/utility.js
+++ reactburgerbuilder/src/shared/utility.js
@@ -4,3 +4,23 @@ export const updateObject = (oldObject, updatedProperties) => {
         ...updatedProperties
     };
 };
+
+export const checkValidity = (value, rules) => {
+    let isValid = true;
+    if (!rules) {
+        return true;
+    }
+
+    if (rules.required){
+        isValid = value !== '' && isValid;
+    }
+    if (rules.minlLength) {
+        isValid = value.length >= rules.minlLength && isValid
+    }
+    if (rules.maxlLength) {
+        isValid = value.length <= rules.maxlLength && isValid
+    }
+  
+
+    return isValid;
+}
