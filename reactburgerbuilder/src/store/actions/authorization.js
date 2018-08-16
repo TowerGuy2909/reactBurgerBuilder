a2d21cdd08bba91e6276475849a2b623af089752
Jerridd Speidel <jerridd_speidel@yahoo.com>
2018-08-16 10:27:17 -0500

    checking auth state and storing in local store with token and id

--- reactburgerbuilder/src/store/actions/authorization.js
+++ reactburgerbuilder/src/store/actions/authorization.js
@@ -32,6 +32,9 @@ export const checkAuthorizationTimeout = (expirationTime) => {
 };
 
 export const logout = () => {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationDate');
+    localStorage.removeItem('userId');
     return {
         type: actionTypes.AUTHORIZATION_LOGOUT
     }
@@ -52,6 +55,10 @@ export const authorization = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
+                const expirationDate = new Date(new Date().getTime() + response.data.expiresIn * 1000);
+                localStorage.setItem('token', response.data.idToken);
+                localStorage.setItem('expirationDate', expirationDate);
+                localStorage.setItem('userId', response.data.userId);
                 dispatch(authorizationSuccess(response.data.idToken, response.data.localId));
                 dispatch(checkAuthorizationTimeout(response.data.expiresIn));
             })
@@ -65,5 +72,23 @@ export const setAuthRedirectPath = (path) => {
     return {
         type: actionTypes.SET_AUTH_REDIRECT_PATH,
         path: path
+    }
+}
+
+export const authCheckState = () => {
+    return dispatch => {
+        const token = localStorage.getItem('token');
+        if (!token) {
+            dispatch(logout());
+        } else {
+            const expirationDate = localStorage.getItem('expirationDate')
+            if (expirationDate > new Date()) {
+                dispatch(logout());
+            }else {
+                const userId = localStorage.getItem('userId');
+                dispatch(authorizationSuccess(token, userId));
+                dispatch(checkAuthorizationTimeout(expirationDate.getSeconds() - new Date().getSeconds()))
+            }
+        }
     }
 }
